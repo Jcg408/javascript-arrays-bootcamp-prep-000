faec0d2aae79863413b8ea0b1b9a057610565640
Jessica Gladstone <jcg321@comcast.net>
2018-09-08 18:41:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array){
-    array.slice(1);
+    var arr = array.slice(1);
+    return arr;
    
 }
 
